27e0fe21df4cb26ccd4d71da54a5781650ea01f3
Rafael Dutra <raffaeldutra@gmail.com>
2019-04-18 19:43:27 -0300

    Nao mais necessario

--- runme.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Rafael Dutra <raffaeldutra@gmail.com>
-# http://rafaeldutra.me
-
-docker build -t fossday/gohugo .
-
-docker run --rm -detached --volume $(pwd)/site:/go/fossday fossday/gohugo /go/bin/hugo new site fossday
-
-git clone https://github.com/jweslley/hugo-conference.git site/themes/
