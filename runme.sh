5b0b89a88d3ce58ea30cad46c9fcb7b8289e31d1
rtomkiel <regis@doseextra.com>
2019-04-18 06:32:43 -0300

    Adicionada grade de palestrantes LJ 2019, correções e otimização de ressources

--- runme.sh
+++ runme.sh
@@ -2,8 +2,13 @@
 # Rafael Dutra <raffaeldutra@gmail.com>
 # http://rafaeldutra.me
 
-docker build -t fossday/gohugo .
+#docker build -t fossday/gohugo .
 
-docker run --rm -detached --volume $(pwd)/site:/go/fossday fossday/gohugo /go/bin/hugo new site fossday
+#docker run --rm -detached --volume $(pwd)/site:/go/fossday fossday/gohugo /go/bin/hugo new site fossday
 
-git clone https://github.com/jweslley/hugo-conference.git site/themes/
+#git clone https://github.com/jweslley/hugo-conference.git site/themes/
+
+docker run -it \
+-v $(pwd)/site:/src \
+-p 1313:1313 \
+raffaeldutra/docker-gohugo /gohugo.sh
